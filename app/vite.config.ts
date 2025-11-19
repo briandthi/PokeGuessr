c04f61d5abb8dbb6a7d23e0940120666aa98a024
Thibaut <briandthi@gmail.com>
2025-11-19 19:18:47 +0100

    init serveur privé

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   }, server: {
-    allowedHosts: ['pokeguessr.thibaultbriand.fr']
+    allowedHosts: ['pokeguessr.tbriand.com']
   }
 });
